f4014316af5aa206a45b12761b73348f4f4006b8
Discords Partner <74417817+discordspartner@users.noreply.github.com>
2021-10-16 00:08:58 +0530

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,18 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: '--TOKEN--',
+        prefix: '--PREFIX--',
+        activity: '--BOT ACTIVITY--',
+        
+        
+    },
+
+};

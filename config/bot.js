f0c9462eb403aa650b256a076a6b8617802717bb
/parTner/ <74417817+discordspartner@users.noreply.github.com>
2021-11-09 01:22:54 +0530

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,19 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'Nzg0MDgzMzQzNjk5NjA3NTc0.X8kIhA.XZwKv7JCvYgljMu1U0hZ-UjHOjs',
+        prefix: '??',
+        activity: 'APEX LEGENDS',
+        
+        
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};

c9ed6b0c0c8dc1c9869dfb27f007ba1b96308ff6
/parTner/ <74417817+discordspartner@users.noreply.github.com>
2021-11-09 01:08:45 +0530

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -14,5 +14,5 @@ module.exports = {
         
         
     },
-
+        filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
 };
